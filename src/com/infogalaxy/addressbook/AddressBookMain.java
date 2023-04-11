dcb927e7f504f2b5e8dd12c51c8786409dfc5551
TechnoSpaceIT <technospaceitclasses@gmail.com>
2023-04-11 20:41:26 +0530

    [ADDED]:[Tahir] Address Book Project Setup with Welcome Message

---
+++ src/com/infogalaxy/addressbook/AddressBookMain.java
@@ -0,0 +1,21 @@
+package com.infogalaxy.addressbook;
+
+/***
+ * 
+ * @author DELL
+ * AddressBookMain Class is used for performing different 
+ * Operations on AddressBook
+ */
+public class AddressBookMain {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		/**
+		 * Welcome message for user
+		 */
+		System.out.println("Welcome to AddressBook Program");
+		
+	}
+
+}
