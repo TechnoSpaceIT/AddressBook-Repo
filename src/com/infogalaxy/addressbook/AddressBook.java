30f92ba49105cd9a1545f6471eaa4b926b8b0d3b
TechnoSpaceIT <technospaceitclasses@gmail.com>
2023-04-12 21:26:06 +0530

    [MODIFIED]:[Tahir] Update Contact using First Name

--- src/com/infogalaxy/addressbook/AddressBook.java
+++ src/com/infogalaxy/addressbook/AddressBook.java
@@ -40,5 +40,28 @@ public class AddressBook {
 		System.out.println("Phone Number:"+contacts.getPhoneNumber());
 		System.out.println("EMail ID:"+contacts.getEmail());
 	}
-	
+	public void updateContact() {
+		System.out.println("Enter the First Name to Update Contact:");
+		String fname = sc.next();
+		if(fname.equals(contacts.getFirstName())) {
+			System.out.println("Contact available");
+			System.out.println("Enter the Last Name:");
+			contacts.setLastName(sc.next());
+			System.out.println("Enter the Address:");
+			contacts.setAddress(sc.next());
+			System.out.println("Enter the City:");
+			contacts.setCity(sc.next());
+			System.out.println("Enter the State:");
+			contacts.setState(sc.next());
+			System.out.println("Enter the Zipcode:");
+			contacts.setZip(sc.next());
+			System.out.println("Enter the Phone Number:");
+			contacts.setPhoneNumber(sc.next());
+			System.out.println("Enter the EMail ID:");
+			contacts.setEmail(sc.next());
+			
+		} else {
+			System.out.println("Contact is Not Available");
+		}
+	}
 }
--- src/com/infogalaxy/addressbook/AddressBookMain.java
+++ src/com/infogalaxy/addressbook/AddressBookMain.java
@@ -1,5 +1,7 @@
 package com.infogalaxy.addressbook;
 
+import java.util.Scanner;
+
 /***
  * 
  * @author DELL
@@ -16,9 +18,34 @@ public class AddressBookMain {
 		 */
 		System.out.println("Welcome to AddressBook Program");
 		
+		Scanner sc = new Scanner(System.in);
+		int choice;
+		
 		AddressBook addressBook = new AddressBook();
-		addressBook.addContact();
-		addressBook.showContact();
+		
+		do
+		{
+			System.out.println("***ADDRESSBOOK MENU***");
+			System.out.println("1. ADD CONTACT \n2. SHOW CONTACT \n3. UPDATE CONTACT \n4. DELETE CONTACT");
+			System.out.println("Enter the CHOICE:");
+			choice = sc.nextInt();
+			switch(choice) {
+			case 1:
+				addressBook.addContact();
+				break;
+			case 2:
+				addressBook.showContact();
+				break;
+			case 3:
+				addressBook.updateContact();
+				break;
+			case 4:
+				break;
+			default:
+				System.out.println("Invalid Choice");
+					break;
+			}
+		}while(choice<5);
 	}
 
 }
